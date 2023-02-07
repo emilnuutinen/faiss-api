03166d96e50a38aa259d78212795719a07d52475
Emil Nuutinen <emil@emilnuutinen.com>
2023-02-07 21:01:24 +0200

    Add Epsilon address back

--- query.py
+++ query.py
@@ -1,13 +1,14 @@
 import requests
 
-limit = 20
+limit = 200000
 
 while True:
     print()
     query = input('Your query: ')
 
-    results = requests.get(f'http://0.0.0.0:8791/v2/l={limit}&q={query}')
-    
+    results = requests.get(
+        f'http://epsilon-it.utu.fi/ecco-api/v2/l={limit}&q={query}')
+
     for result in results.json():
         print()
         print(f'Book: {result["id"]}')
